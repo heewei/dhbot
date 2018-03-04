07996415765d4ca92476e2121eeb5987be8fbc9d
hewei <heewei84@gmail.com>
2018-03-04 03:08:52 +0000

    Unit TEst servo working. PWM pulse not working

--- UnitTest.py
+++ UnitTest.py
@@ -4,13 +4,16 @@ import pigpio #take ntoe pigpio uses BCM
 pi1 = pigpio.pi() #local gpio
 
 pi1.set_mode(14, pigpio.OUTPUT)
+pi1.set_mode(15, pigpio.OUTPUT)
 
 pi1.set_PWM_frequency(24, 100)
+pi1.set_servo_pulsewidth(15, 500)
 
-pi1.set_PWM_dutycycle(23, 10)
+pi1.set_PWM_dutycycle(23, 20)
 
 time.sleep(3)
+pi1.set_servo_pulsewidth(15, 2500)
 
 #cleanup
-pi.stop() 
+pi1.stop() 
 
