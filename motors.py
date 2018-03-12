9381f9ff62b92502a13177e64b877f6d39dc8891
hewei <heewei84@gmail.com>
2018-03-12 05:41:07 +0000

    rename motor and corrected motor code

--- motors.py
+++ motors.py
@@ -23,27 +23,27 @@ class Motors:
 
     def setPowerL(self, val):
         val = checkVal(val)       
-		self.pi.write(LDir, setDir(val))
-		self.powerL = val * 255				
+	self.pi.write(LDir, setDir(val))
+	self.powerL = val * 255				
         print("pwrL=", self.powerL)
         self.pi.set_PWM_dutycycle(LPWM, self.powerL)
         
     def setPowerR(self, val):
-        val = checkVal(val)       
-		self.pi.write(RDir, setDir(val))
-		self.powerR = val * 255				
+        val = checkVal(val)
+	self.pi.write(RDir, setDir(val))
+	self.powerR = val * 255				
         print("pwrR=", self.powerR)
         self.pi.set_PWM_dutycycle(RPWM, self.powerR)        
     
     def checkVal(value):
-		if value > 1.0:
-			value = 1.0
-		elif value < -1.0:
-			value = -1.0
-		return value
-		
-	def setDir(value):
-		if value < 0:
-			return REVERSE
-		else
-			return FORWARD
+	if value > 1.0:
+		value = 1.0
+	elif value < -1.0:
+		value = -1.0
+	return value
+    
+    def setDir(value):
+	val = FORWARD
+	if value < 0:
+		val = REVERSE
+	return val
