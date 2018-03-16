93c84026c5fbf5ec3a493028fd803d95e855c30f
hewei <heewei84@gmail.com>
2018-03-16 12:43:37 +0000

    added test machine framework

--- gundam.py
+++ gundam.py
@@ -5,9 +5,10 @@ import robotBuilder
 from timer import Timer
 from math import pi, degrees
 
-
 #create robot instance 
 myRobot = robotBuilder.build()
+#REST engine?
+
 
 #set timestep
 tStep = 0.02
@@ -15,6 +16,18 @@ tStep = 0.02
 try:
 
     finished = False
+    while not finished:
+		#check state
+		if state is 1:
+			print("Autonomous Moving")#move
+		elif state is 2:
+			print("Relative moving")
+		elif state is 3:
+			print("Starting mapping")#start mapping engine
+		elif state is 4:
+			print("Self Test")#diagnostics 
+			
+		
     
 except (KeyboardInterrupt, SystemExit):
     print("Keyboard Interrupt")
